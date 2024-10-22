00e0a7ccd3e289b9218e64bdbd7629c0b3b1b2ab
KhanhRomVN <87371837+KhanhRomVN@users.noreply.github.com>
2024-10-22 17:27:20 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # helmet-vehicle-detection
+
+dataset: https://universe.roboflow.com/checkhelmet/helmetcheck/dataset/1
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
